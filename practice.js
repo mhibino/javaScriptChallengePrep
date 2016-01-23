edb60f4a9305e2d43637ea20f9b70e036c26e3d2
Michiya Hibino <mphibino@gmail.com>
2016-01-23 16:27:45 -0500

    added fibonacci generator

--- practice.js
+++ practice.js
@@ -1,20 +1,21 @@
 
+function testPrime(num) {
+	if (num === 1) {
+		return false;
+	}
+	for (var i = 2; i < num/2; i++) {
+		if (num % i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 //1. Generate nth prime number
 function primeGenerator(n) {
 	var result = [];
 	var counter = 2;
-	function testPrime(num) {
-		if (num === 1) {
-			return false;
-		}
-		for (var i = 2; i < num/2; i++) {
-			if (num % i === 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-
+	
 	while (result.length < n) {
 		if (testPrime(counter)) {
 			result.push(counter);
@@ -25,7 +26,45 @@ function primeGenerator(n) {
 }
 
 
-// 2. Generate prime numbers up to n
+// 2. Generate prime numbers up to number n
+function primesToN(n) {
+	var result = [];
+	for (var i = 2; i <= n; i++) {
+		if (testPrime(i)) {
+			result.push(i);
+		}
+	}
+	return result;
+}
+
+// fibonacci [0, 1, 1, 2, 3, 5, 8...]
+function fibonacciGenerator(n) {
+	var result = [0, 1];
+	var sum;
+	var a = 0;
+	var b = 1;
+	var next = a + b;
+
+	if (n === 0) {
+		return [];
+	} else if (n === 1) {
+		return [0];
+	} else {
+		for(var i = 1; i <= n-2; i++) {
+			sum = a + b;
+			result.push(sum);
+			a = b;
+			b = sum;
+		}
+		
+	}
+	return result;
+}
+
+fibonacciGenerator(4);
+
+
+
 
 
 
