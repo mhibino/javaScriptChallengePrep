d9f8d36ec32cacac6168f64690fa519f2ef04cb9
Michiya Hibino <mphibino@gmail.com>
2016-01-26 23:50:53 -0500

    refactored fibonacci generator functions

--- practice.js
+++ practice.js
@@ -1,4 +1,7 @@
 
+// CHALLENGE A: PRIME NUMBER FUNCTIONS:
+
+// PRIME 1: Prime Number Tester (and Helper Function)
 function testPrime(num) {
 	if (num === 1) {
 		return false;
@@ -11,7 +14,7 @@ function testPrime(num) {
 	return true;
 }
 
-//1. Generate nth prime number
+// PRIME 2: Prime Number Generator: Generate n number of prime number outputs
 function primeGenerator(n) {
 	var result = [];
 	var counter = 2;
@@ -25,8 +28,7 @@ function primeGenerator(n) {
 	return result;
 }
 
-
-// 2. Generate prime numbers up to number n
+// PRIME 3: Generate prime numbers up to number n (inclusive)
 function primesToN(n) {
 	var result = [];
 	for (var i = 2; i <= n; i++) {
@@ -37,62 +39,65 @@ function primesToN(n) {
 	return result;
 }
 
-// fibonacci [0, 1, 1, 2, 3, 5, 8...]
-function fibonacciGenerator(n) {
-	var result = [0, 1];
-	var sum;
-	var a = 0;
-	var b = 1;
-	var next = a + b;
 
-	if (n <= 0) {
-		return [];
-	} else if (n === 1) {
-		return [0];
-	} else {
-		for(var i = 1; i <= n-2; i++) {
-			sum = a + b;
-			result.push(sum);
-			a = b;
-			b = sum;
-		}
-		
-	}
-	return result;
+/*
+________________________________________________________________
+________________________________________________________________
+*/
+
+
+// CHALLENGE B: FIBONACCI SEQUENCE: [0, 1, 1, 2, 3, 5, 8...]
+
+// Fibonacci Challenge 1: 
+// Create function fibGen(n) that takes input n, and outputs fibonacci sequence that is n integers long
+function fibGen(n) {
+    var fibSeq = [];
+    var a = 0;
+    var b = 1;
+    var c = 1;
+    for (var i = 1; i <= n; i++) {
+        fibSeq.push(a);
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return fibSeq;
 }
+// Test Cases for fibGen()
+console.log(fibGen(-1)); // Returns []
+console.log(fibGen(0)); // Returns []
+console.log(fibGen(1)); // Returns [ 0 ]
+console.log(fibGen(2)); // Returns [ 0, 1 ]
+console.log(fibGen(5)); // Returns [ 0, 1, 1, 2, 3 ]
+console.log(fibGen(10)); // Returns [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
+console.log(fibGen(15)); // Returns [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 ]
 
-fibonacciGenerator(4); // [0, 1, 1, 2]
-
-
-// Generate fibonacci numbers up to number n
-function fibonacciToN(n) {
-	var result = [0, 1, 1];
-	var a = 1;
-	var b = 1;
-	var sum = -1;
-	if (n < 0) {
-		return [];
-	} else if (n === 0) {
-		return [0];
-	} else if (n === 1) {
-		return [0, 1, 1];
-	} else {
-		while (sum <= n) {
-			sum = a + b;
-			if (sum <= n) {
-				result.push(sum);
-			}
-			a = b;
-			b = sum;
-		}
-	}
-	return result;
+// Fibonacci Challenge 2: 
+// Create function fibonacciToN(n) that generates fibonacci sequence up to number n (inclusive)
+function fibToN(n) {
+    var fibSeq = [];
+    var a = 0;
+    var b = 1;
+    var c = 1;
+    while (a <= n) {
+        fibSeq.push(a);
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return fibSeq;
 }
+// Test Cases for fibToN()
+console.log(fibToN(-1)); // Returns []
+console.log(fibToN(0)); // Returns [ 0 ]
+console.log(fibToN(1)); // Returns [ 0, 1, 1 ]
+console.log(fibToN(2)); // Returns [ 0, 1, 1, 2 ]
+console.log(fibToN(5)); // Returns [ 0, 1, 1, 2, 3, 5 ]
+console.log(fibToN(10)); // Returns [ 0, 1, 1, 2, 3, 5, 8 ]
+console.log(fibToN(100)); // Returns [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ]
 
 
-
-
-
+// LINK TO Fibonacci Code (which includes earlier attempt alongside Refactored Code): https://repl.it/BgZS/4
 
 
 
